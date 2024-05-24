a06c660f1e652836eab86c3f229b8bad1a47ccca
IamGursimar <gursimarsingh195@gmail.com>
2024-05-24 13:40:30 +0800

    pass stage threading

--- app/main.py
+++ app/main.py
@@ -1,14 +1,20 @@
 import socket
+import threading
+
+
+def handle_conn(conn):
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            conn.send(b"+PONG\r\n")
 
 
 def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept()  # wait for client
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            conn.send(b"+PONG\r\n")
+    while True:
+        conn, _ = server_socket.accept()  # wait for client
+        threading.Thread(target=handle_conn, args=(conn,)).start()
 
 
 if __name__ == "__main__":

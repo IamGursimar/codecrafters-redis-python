1c5292b728b0ca8312bbb41e98bd386866271224
IamGursimar <gursimarsingh195@gmail.com>
2024-05-25 16:33:02 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 import socket
 import threading
 
+"""
+# TODO: Parse RESP.
+# TODO: Support for ECHO command (Encoded as RESP bulk string). No hardcoding.
+# TODO: Implement redis protocol parser.
+# TODO: Input which will be encoded using RESP.
+
+"""
+
 
 def handle_conn(conn):
     with conn:
         while True:
-            data = conn.recv(1024)
-            conn.send(b"+PONG\r\n")
+            data = conn.recv(1024).decode()
+            # "*1\r\n$4\r\nPING"
+            response = b"+PONG\r\n"
+            data = data.split("\r\n")
+            ["*2", "$4", "ECHO", "$6", "orange", ""]
+            if data[2].lower() == "echo":
+                conn.send(f"+{data[4]}\r\n".encode())
+            else:
+                conn.send(response)
 
 
 def main():

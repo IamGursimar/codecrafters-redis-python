4019ccf028b3162d949ff78cf57323e2a7b545ab
IamGursimar <gursimarsingh195@gmail.com>
2024-06-06 19:42:52 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,27 +1,33 @@
 import socket
 import threading
 
-"""
-# TODO: Parse RESP.
-# TODO: Support for ECHO command (Encoded as RESP bulk string). No hardcoding.
-# TODO: Implement redis protocol parser.
-# TODO: Input which will be encoded using RESP.
-
-"""
+storage_dict = {}
 
 
 def handle_conn(conn):
     with conn:
         while True:
             data = conn.recv(1024).decode()
+            if not data:
+                break
             # "*1\r\n$4\r\nPING"
-            response = b"+PONG\r\n"
+            # response = b"+PONG\r\n"
             data = data.split("\r\n")
-            ["*2", "$4", "ECHO", "$6", "orange", ""]
-            if data[2].lower() == "echo":
-                conn.send(f"+{data[4]}\r\n".encode())
-            else:
-                conn.send(response)
+            # Order is as follows * number of strings, $ len of things for each.
+            # ["*2", "$4", "ECHO", "$6", "orange", ""]
+            if data[2].lower() == "ping":
+                conn.send(b"+PONG\r\n")
+            elif data[2].lower() == "echo":
+                response = f"+{data[4]}\r\n"
+                conn.send(response.encode())
+            elif data[2].lower() == "set":
+                storage_dict[data[4]] = data[6]
+                conn.send(b"+OK\r\n")
+            elif data[2].lower() == "get":
+                return_value = storage_dict[data[4]]
+                conn.send(f"${len(return_value)}\r\n{return_value}\r\n".encode())
+            # else:
+            #     conn.send(response)
 
 
 def main():
